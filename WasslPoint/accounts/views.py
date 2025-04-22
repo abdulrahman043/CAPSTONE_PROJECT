1446c059de4139d25a04f77a093f04da67b52694
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-04-22 13:50:31 +0300

    login and register

--- WasslPoint/accounts/views.py
+++ WasslPoint/accounts/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpRequest
+from django.db import transaction
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from profiles.models import CompanyProfile,StudentProfile,PersonalInformation
 # Create your views here.
+
+def signup_view(request:HttpRequest):
+    if request.method=='POST':
+        with transaction.atomic():
+            try:
+                user = User.objects.create_user(username=request.POST["email"], password=request.POST["password"])
+                user.save()
+                profile=StudentProfile.objects.create(user=user)
+                PersonalInformation.objects.create(profile=profile,full_name_ar=request.POST['full_name'])
+                return redirect('accounts:login_view')
+            except Exception as e:
+                print(e)
+    return render(request,'accounts/signup.html')
+
+
+def signup_company_view(request:HttpRequest):
+    if request.method=='POST':
+        print(request.POST)
+        with transaction.atomic():
+            try:
+                user = User.objects.create_user(username=request.POST["email"], password=request.POST["password"])
+                user.save()
+                CompanyProfile.objects.create(user=user,commercial_register=request.POST['commercial_register'])
+                return redirect('accounts:login_view')
+            except Exception as e:
+                print(e)
+
+    return render(request,'accounts/signup_company.html')
+def login_view(request:HttpRequest):
+    if request.method=='POST':
+        user=authenticate(request,username=request.POST["email"],password=request.POST['password'])
+        if user is not None:
+            login(request,user)
+            return redirect('main:home_view')
+    return render(request,'accounts/login.html')
+def logout_view(request:HttpRequest):
+    logout(request)
+
+    return redirect('main:home_view')
