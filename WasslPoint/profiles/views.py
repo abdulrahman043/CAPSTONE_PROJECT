8c1bf6f45e4a975c29922897455b953a777f3357
abdulrahman <adda.1232@gmail.com>
2025-04-24 09:46:01 +0300

    edit on profile.html

--- WasslPoint/profiles/views.py
+++ WasslPoint/profiles/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import PersonalInformation,Country
 # Create your views here.
 def profile_view(request:HttpRequest):
-    return render(request,'profiles/profile.html')
+    countries = Country.objects.filter(status=True)  # only active ones
+
+    context = {
+    "gender_choices": PersonalInformation.Gender.choices,
+    'countries':countries,
+}
+    return render(request,'profiles/profile.html',context)
