b55303791ab7ffd11ed478edee236b5280140efe
Ahmed Alghamdi <moodya793@gmail.com>
2025-04-27 00:59:19 +0300

    fourth edit

--- WasslPoint/main/urls.py
+++ WasslPoint/main/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 app_name = "main"
-urlpatterns = [path('',views.home_view,name='home_view')]
+urlpatterns = [
+    path('',views.home_view,name='home_view'),
+    path('training/',views.training_view,name='training_view'),
+    path('about/',views.about_view,name='about_view'),
+    path('contact/',views.contact_view,name='contact_view'),
+]
+
+
--- WasslPoint/main/views.py
+++ WasslPoint/main/views.py
@@ -3,3 +3,13 @@ from django.http import HttpRequest
 # Create your views here.
 def home_view(request:HttpRequest):
     return render(request,'main/home.html')
+
+def training_view(request:HttpRequest):
+    return render(request,'main/training.html')
+
+def about_view(request:HttpRequest):
+    return render(request,'main/about.html')
+
+def contact_view(request:HttpRequest):
+    return render(request,'main/contact.html')
+
