09162b70d78f4732263ed77cb346c8cff0216125
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-04-27 13:15:55 +0300

    admin user-list ,  pdf export in student profile

--- WasslPoint/profiles/views.py
+++ WasslPoint/profiles/views.py
@@ -5,7 +5,7 @@ from datetime import date
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.template.loader import render_to_string
-#from weasyprint import HTML
+from weasyprint import HTML
 # Create your views here.
 @login_required
 def profile_view(request:HttpRequest):
@@ -344,13 +344,13 @@ def company_profile_view(request:HttpRequest):
 
 @login_required
 def export_cv_pdf(request):
-    pass
-    # try:
-    #     html=render_to_string('profiles/cv_pdf.html',{'request':request})
-    #     response=HttpResponse(content_type='application/pdf')
-    #     response['Content-Disposition']=f'filename={request.user.username}_cv.pdf'
-    #     HTML(string=html, base_url=request.build_absolute_uri('/')).write_pdf(response)
-    #     return response
+    
+    try:
+        html=render_to_string('profiles/cv_pdf.html',{'request':request})
+        response=HttpResponse(content_type='application/pdf')
+        response['Content-Disposition']=f'filename={request.user.username}_cv.pdf'
+        HTML(string=html, base_url=request.build_absolute_uri('/')).write_pdf(response)
+        return response
 
-    # except Exception as e:
-    #     print(e)
+    except Exception as e:
+        print(e)
