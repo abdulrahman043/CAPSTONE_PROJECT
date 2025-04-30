d65435ca1afd72eec8cfc7d8d212ff39be00360d
MohammedAAlbushaier <moh.albushaier@gmail.com>
2025-04-30 22:17:05 +0300

    updateing the posts App,and the company.html they still need some improvments and fixes

--- WasslPoint/posts/admin.py
+++ WasslPoint/posts/admin.py
@@ -1,7 +1,2 @@
-# WasslPoint/posts/admin.py
 from django.contrib import admin
-from .models import TrainingOpportunity, Application, Message
-
-admin.site.register(TrainingOpportunity)
-admin.site.register(Application)
-admin.site.register(Message)
+from .models import TrainingOp
