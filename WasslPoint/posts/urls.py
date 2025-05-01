ef063aa35815c8930317b9b0a263d7e24ba5589d
MohammedAAlbushaier <moh.albushaier@gmail.com>
2025-05-01 10:59:06 +0300

    Update urls.py
    Fixing URLS

--- WasslPoint/posts/urls.py
+++ WasslPoint/posts/urls.py
@@ -4,3 +4,13 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
+    path('', views.opportunity_list, name='opportunity_list'),
+    path('company/opportunities/', views.company_opportunities, name='company_opportunities'),
+    path('opportunity/create/', views.create_opportunity, name='create_opportunity'),
+    path('opportunity/<int:opportunity_id>/', views.opportunity_detail, name='opportunity_detail'),
+    path('opportunity/<int:opportunity_id>/apply/', views.apply_opportunity, name='apply_opportunity'),
+    path('applications/', views.application_status, name='application_status'),
+    path('opportunity/<int:opportunity_id>/applications/', views.opportunity_applications, name='opportunity_applications'),
+    path('application/<int:application_id>/status/update/', views.update_application_status, name='update_application_status'),
+    path('application/<int:application_id>/chat/', views.application_chat, name='application_chat'),
+]
