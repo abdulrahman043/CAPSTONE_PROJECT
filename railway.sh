160655b9bbe204ea3f1af27bf254f2c1225d7143
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-05-08 02:13:26 +0300

    deploy

--- railway.sh
+++ railway.sh
@@ -1,7 +1,8 @@
 {
   "$schema": "https://railway.app/railway.schema.json",
   "build": {
-    "builder": "Nixpacks"
+    "builder": "Nixpacks",
+    "appPath": "WasselPoint"
   },
   "deploy": {
     "startCommand": "cd WasselPoint && python manage.py migrate --noinput && python manage.py collectstatic --noinput && gunicorn WasselPoint.wsgi:application --bind 0.0.0.0:$PORT"
