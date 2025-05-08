b80d6f65697dd562948eb2bfb03f5221b62077c2
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-05-08 11:20:09 +0300

    last

--- WasslPoint/WasslPoint/settings.py
+++ WasslPoint/WasslPoint/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -58,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 ROOT_URLCONF = 'WasslPoint.urls'
 
@@ -140,7 +140,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
