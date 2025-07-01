de10f7d8b1a0a0bc0d97630acab999434365f6da
abdulrahman <adda.1232@gmail.com>
2025-07-01 21:27:33 +0300

    deploy

--- WasslPoint/WasslPoint/settings.py
+++ WasslPoint/WasslPoint/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-load_dotenv()
-print("PGHOST =", os.getenv("PGHOST"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STRIPE_SECRET_KEY       = os.getenv("STRIPE_SECRET_KEY")
