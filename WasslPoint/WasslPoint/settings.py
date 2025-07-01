80ca56723551d92ad9657085a09eb7414c4fb189
abdulrahman <adda.1232@gmail.com>
2025-07-01 21:24:45 +0300

    deploy

--- WasslPoint/WasslPoint/settings.py
+++ WasslPoint/WasslPoint/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
-print(os.getenv('t'))
+print("PGHOST =", os.getenv("PGHOST"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STRIPE_SECRET_KEY       = os.getenv("STRIPE_SECRET_KEY")
