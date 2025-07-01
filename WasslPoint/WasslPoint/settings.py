7129e8875eb4ec8460c3b0695530fad74500fb0a
abdulrahman <adda.1232@gmail.com>
2025-07-01 21:12:18 +0300

    deploy

--- WasslPoint/WasslPoint/settings.py
+++ WasslPoint/WasslPoint/settings.py
@@ -15,6 +15,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 print(os.getenv("CLOUDFLARE_R2_BUCKET_ENDPOINT"))
+print(os.getenv("STRIPE_SECRET_KEY"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
